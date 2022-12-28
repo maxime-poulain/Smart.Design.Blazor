51892842ffec447e1495a0c043908ffa295ac509
Maxime Poulain <maxime.poulain@outlook.be>
2022-12-28 05:55:26 +0100

    Remove projects to test the library

--- BlazorApp1/Extensions/JsonExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.Json;
-
-namespace BlazorApp1.Extensions;
-
-public static class JsonExtensions
-{
-    public static string ToJson(this object source, JsonSerializerOptions? options = default) => JsonSerializer.Serialize(source, options);
-}
